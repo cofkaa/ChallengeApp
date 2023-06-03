35e653a2985795f97acd1b9f81221f2f32ed793b
Agnieszka Kuźniarz <agnieszka.kuzniarz@abas-poland.com>
2023-06-03 21:01:28 +0200

    Dzień 4 - Zmienne i podstawowoe operacje

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,5 +1,26 @@
-﻿
-Console.WriteLine("");
-Console.WriteLine("*************************");
-Console.WriteLine("*     Hello Aga!        *");
-Console.WriteLine("*************************");
+﻿using System.Globalization;
+
+string sName = "Aga";
+char cGender = 'K';
+int iAge = 20;
+
+string sText1 = "Kobieta poniżej 30 lat";
+string sText2 = "Ewa, lat 33";
+string sText3 = "Niepełnoletni mężczyzna";
+
+if (cGender == 'K')
+{
+    if (iAge < 30)
+    {
+        Console.WriteLine(sText1);
+    }
+    else if (sName == "Ewa" && iAge == 33)
+    {
+        Console.WriteLine(sText2);
+    }
+}
+else if (iAge < 18)
+{
+    Console.WriteLine(sText3);
+}
+
