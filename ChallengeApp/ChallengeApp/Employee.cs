7d83d403ea15a022514b7e996136be76016ec1b6
cofkaa <cofkaa@o2.pl>
2023-06-05 21:50:37 +0200

    Dzień 10 - Pamięć, walidacja i rzutowanie

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -1,11 +1,13 @@
-﻿namespace ChallengeApp
+﻿using System.Runtime.InteropServices;
+
+namespace ChallengeApp
 {
     public class Employee
     {
         public string Name { get; private set; }
         public string Surname { get; private set; }
         public int Age { get; private set; }
-        
+
         private List<float> grades = new List<float>();
 
         public Employee(string name, string surname, int age)
@@ -14,12 +16,41 @@
             this.Surname = surname;
             this.Age = age;
         }
+        public void AddGrade(string grade)
+        {
+            Console.WriteLine("grade string:" + grade);
+            if (float.TryParse(grade, out float result))
+                AddGrade(result);
+            else
+                Console.WriteLine("** string is not float");
+        }
+        public void AddGrade(decimal grade)
+        {
+            Console.WriteLine("grade decimal:" + grade);
+            AddGrade((float)grade);
+        }
+
+        public void AddGrade(double grade)
+        {
+            Console.WriteLine("grade double:" + grade);
+            AddGrade((float)grade);
+        }
+
+        public void AddGrade(long grade)
+        {
+            Console.WriteLine("grade long:" + grade);
+            AddGrade((float)grade);
+        }
 
         public void AddGrade(float grade)
         {
-            this.grades.Add(grade);
+            Console.WriteLine("grade float:" + grade);
+            if (grade >= 0 && grade <= 100)
+                this.grades.Add(grade);
+            else
+                Console.WriteLine("** invalid grade value");
         }
-        
+
         public Statistics GetStatistics()
         {
             var statistics = new Statistics();
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -5,20 +5,44 @@ employees.Add(new Employee("Adam", "Kowalski", 35));
 employees.Add(new Employee("Kasia", "Nowak", 22));
 employees.Add(new Employee("Jola", "Woźniak", 50));
 
-int newGrade;
-Random random = new();
+//int newGrade;
+//Random random = new();
 foreach (var employee in employees)
 {
     Console.WriteLine($"{employee.Name} {employee.Surname} ({employee.Age})");
-    for (int i = 0; i < random.Next(0, 10); i++)
-    {
-        newGrade = random.Next(1, 101);
-        Console.WriteLine(newGrade);
-        employee.AddGrade(newGrade);
-    }
+    //for (int i = 0; i < random.Next(0, 10); i++)
+    //{
+    //    newGrade = random.Next(0, 101);
+    //    Console.WriteLine(newGrade);
+    //    employee.AddGrade(newGrade);
+    //}
+    Console.WriteLine("-----------------------");
+    employee.AddGrade("20,1");
+    Console.WriteLine("-----------------------");
+    employee.AddGrade(1);
+    Console.WriteLine("-----------------------");
+    employee.AddGrade(0.423e-40);
+    Console.WriteLine("-----------------------");
+    employee.AddGrade(10.555f);
+    Console.WriteLine("-----------------------");
+    employee.AddGrade(1.5654E1m);
+    Console.WriteLine("-----------------------");
+    employee.AddGrade(long.MaxValue);
+    Console.WriteLine("-----------------------");
+    employee.AddGrade(decimal.MaxValue);
+    Console.WriteLine("-----------------------");
+    employee.AddGrade(double.MaxValue);
+    Console.WriteLine("-----------------------");
+    employee.AddGrade(int.MaxValue);
+    Console.WriteLine("-----------------------");
+
+
     var statistics = employee.GetStatistics();
     Console.WriteLine($"Average: {statistics.Average:N2}");
     Console.WriteLine($"Min: {statistics.Min}");
     Console.WriteLine($"Max: {statistics.Max}");
     Console.WriteLine("****************************");
+    break;
 }
+
+
