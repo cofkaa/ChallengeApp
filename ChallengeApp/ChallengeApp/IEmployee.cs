a401194b0a90138e2152d790c939ec9f5b795822
cofkaa <cofkaa@o2.pl>
2023-06-08 11:55:20 +0200

    Dzień 15 - Tworzenie interfejsu

---
+++ ChallengeApp/ChallengeApp/IEmployee.cs
@@ -0,0 +1,11 @@
+﻿namespace ChallengeApp
+{
+    public interface IEmployee
+    {
+        string Name { get; set; }
+        string Surname { get; set; }
+        int Age { get; set; }
+        char Gender { get; set; }
+        Statistics GetStatistics();
+    }
+}
---
+++ ChallengeApp/ChallengeApp/Supervisor.cs
@@ -0,0 +1,15 @@
+﻿namespace ChallengeApp
+{
+    internal class Supervisor : IEmployee
+    {
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Surname { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Age { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public char Gender { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Statistics GetStatistics()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
