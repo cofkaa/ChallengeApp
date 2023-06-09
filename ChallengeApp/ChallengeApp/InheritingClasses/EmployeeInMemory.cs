8e84e97aa46f0559827028a4f73b1a4c1f028aa5
cofkaa <cofkaa@o2.pl>
2023-06-09 13:43:13 +0200

    Dzień 17 - Zdarzenia (EVENT)

--- ChallengeApp/ChallengeApp/InheritingClasses/EmployeeInMemory.cs
+++ ChallengeApp/ChallengeApp/InheritingClasses/EmployeeInMemory.cs
@@ -4,28 +4,15 @@ namespace ChallengeApp.InheritingClasses
 {
     public class EmployeeInMemory : EmployeeBase
     {
-        public delegate void WriteMessage(string message);
+        public delegate void GradeAddedDelegate(object sender, EventArgs args);
+
+        public event GradeAddedDelegate GradeAdded;
         public EmployeeInMemory() : base() { }
         public EmployeeInMemory(string name, string surname) : base(name, surname) { }
         public EmployeeInMemory(string name, string surname, char gender) : base(name, surname, gender) { }
         public EmployeeInMemory(string name, string surname, int age) : base(name, surname, age) { }
-        public EmployeeInMemory(string name, string surname, int age, char gender) : base(name, surname, age, gender)
-        {
-            WriteMessage del;
-            del = WriteMessageInConsole;
-            del += WriteMessageInConsoleUpper;
-            del("Mój tekst");
-            del -= WriteMessageInConsole;
-            del("Mój tekst2");
-        }
-        private void WriteMessageInConsole(string message)
-        {
-            Console.WriteLine(message);
-        }
-        private void WriteMessageInConsoleUpper(string message)
-        {
-            Console.WriteLine(message.ToUpper());
-        }
+        public EmployeeInMemory(string name, string surname, int age, char gender) : base(name, surname, age, gender) { }
+
         public override void SayHello()
         {
             Console.WriteLine("HI!!!");
@@ -35,6 +22,10 @@ namespace ChallengeApp.InheritingClasses
         {
             base.AddGrade(grade);
             grades.Add(grade);
+            if (GradeAdded != null)
+            {
+                GradeAdded(this, new EventArgs());
+            }
         }
     }
 }
