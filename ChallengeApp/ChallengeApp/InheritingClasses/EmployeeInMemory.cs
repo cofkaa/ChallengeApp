89e87139a35cad8a03dfe00422351fa262cef930
cofkaa <cofkaa@o2.pl>
2023-06-09 11:49:02 +0200

    Dzień 16 - Delegaty

--- ChallengeApp/ChallengeApp/InheritingClasses/EmployeeInMemory.cs
+++ ChallengeApp/ChallengeApp/InheritingClasses/EmployeeInMemory.cs
@@ -4,12 +4,22 @@ namespace ChallengeApp.InheritingClasses
 {
     public class EmployeeInMemory : EmployeeBase
     {
+        public delegate string WriteMessage(string message);
         public EmployeeInMemory() : base() { }
         public EmployeeInMemory(string name, string surname) : base(name, surname) { }
         public EmployeeInMemory(string name, string surname, char gender) : base(name, surname, gender) { }
         public EmployeeInMemory(string name, string surname, int age) : base(name, surname, age) { }
-        public EmployeeInMemory(string name, string surname, int age, char gender) : base(name, surname, age, gender) { }
-
+        public EmployeeInMemory(string name, string surname, int age, char gender) : base(name, surname, age, gender)
+        {
+            WriteMessage del;
+            del = ReturnMessage;
+            del("Mój tekst");
+            ReturnMessage("Mój tekst");
+        }
+        private string ReturnMessage(string message)
+        {
+            return message;
+        }
         public override void SayHello()
         {
             Console.WriteLine("HI!!!");
